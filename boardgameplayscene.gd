0560692da00f114948c88e54da066842b688ef33
rissarty <aroramanpreet840@gmail.com>
2025-08-05 17:36:03 +0530

    Add files via upload

---
+++ boardgameplayscene.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@onready var tilemap = $TileMap
+@onready var player_piece = $player1spriteGreen
+@onready var rollresult = $rollresult
+
+var tile_map = {} # to be filled with tiles 1 to 100
+
+var current_tile = 1
+
+func _ready():
+	generate_tile_map()
+	player_piece.position = tile_map[current_tile]
+
+# Roll dice
+func _on_dicerollbutton_pressed():
+	var dice_roll = randi() % 6 + 1
+	rollresult.text = "You got " + str(dice_roll)
+
+	var target_tile = current_tile + dice_roll
+	if target_tile > 100:
+		target_tile = 100
+
+	for i in range(current_tile + 1, target_tile + 1):
+		player_piece.position = tile_map[i]
+		await get_tree().create_timer(0.3).timeout
+
+	current_tile = target_tile
+
+# Generate S-pattern tile_map with origin (336, 592)
+func generate_tile_map():
+	var start_x = 336
+	var start_y = 592
+	var tile_size = 32
+	var tile_num = 1
+
+	for row in range(10):
+		var y = start_y - (row * tile_size)
+		for col in range(10):
+			var x = start_x + (col * tile_size)
+			var real_col = col if row % 2 == 0 else 9 - col
+			tile_map[tile_num] = Vector2(start_x + (real_col * tile_size), y)
+			tile_num += 1
